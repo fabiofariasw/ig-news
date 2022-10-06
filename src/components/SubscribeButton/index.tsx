a547bddf73af3f772bfa187e03c704b0ae6668d8
Fabio Farias <fabiofariasw@gmail.com>
2022-10-06 00:45:38 -0300

    feat: módulo jamstack (página estática) aula 2

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,9 @@
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +11,41 @@ interface SubscribeButtonProps {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  console.log(session);
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const {sessionId} = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId})
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
